f5a5ed002cdf75e9834d8c08554bc35bee3d2d84
Gaurav Tiwari <gaurav1@tutamail.com>
2022-02-01 22:22:07 +0530

    Move config loading-parsing to common utils crate

---
+++ services/common/src/utils/configuration.rs
@@ -0,0 +1,61 @@
+use std::convert::{TryFrom, TryInto};
+
+pub fn load_configuration<'a, T: serde::Deserialize<'a>>() -> Result<T, config::ConfigError> {
+    let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("config");
+
+    // Read the "default" configuration file
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    // Detect the running environment.
+    // Default to `development` if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "development".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    // Layer on the environment-specific values.
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
+    // Add in settings from environment variables (with a prefix of MYPASS and '_' as separator)
+    // E.g. `MYPASS_APPLICATION_PORT=5001 would set `Settings.application.port`
+    settings.merge(config::Environment::with_prefix("mypass").separator("_"))?;
+
+    settings.try_into()
+}
+
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Development,
+    Testing,
+    Staging,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Development => "development",
+            Environment::Testing => "testing",
+            Environment::Staging => "staging",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "development" => Ok(Self::Development),
+            "testing" => Ok(Self::Testing),
+            "staging" => Ok(Self::Staging),
+            "production" => Ok(Self::Production),
+            other => Err(format!("{} is not a supported environment.", other)),
+        }
+    }
+}
--- services/user-service/src/main.rs
+++ services/user-service/src/main.rs
@@ -1,10 +1,10 @@
 use user_service::services;
-use user_service::settings;
+use user_service::settings::get_settings;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = settings::get_configuration().expect("Failed to read configuration.");
+    let settings = get_settings().expect("Failed to read configuration.");
 
     // Start Web server
-    services::start_web_service(configuration).await
+    services::start_web_service(settings).await
 }
